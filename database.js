fe3f353c16dae97e066d095b47da34353f22cf86
19BPM <19BPM@19BPM-PC>
2023-12-13 15:08:25 -0300

    database upload

--- database.js
+++ database.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/node-mongodb', { useNewUrlParser: true });
 var conn = mongoose.connection;
 conn.on('connected', function () {
-    console.log('database is connected successfully');
+    console.log('database conectado');
 });
 conn.on('disconnected', function () {
-    console.log('database is disconnected successfully');
+    console.log('database desconectado');
 })
-conn.on('error', console.error.bind(console, 'connection error:'));
+conn.on('error', console.error.bind(console, 'erro de conexão:'));
 module.exports = conn;
